f0f0b78775239c3225c8f65661e1548487f59798
timurturbil <timurturbil@hotmail.com>
2021-01-20 21:35:58 +0300

    Qr Code

---
+++ lib/scan.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ScanPage extends StatefulWidget {
+  @override
+  _ScanPageState createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  Future<void> _launched;
+  FlutterWebviewPlugin flutterWebviewPlugin = FlutterWebviewPlugin();
+  var url = "https://google.com";
+  String qrCodeResult = "Not Yet Scanned";
+/*   @override
+  void initState() {
+    super.initState();
+    flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged wvs) {});
+  } */
+
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+    Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scanner"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              "Result",
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              qrCodeResult,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton(
+              padding: EdgeInsets.all(15.0),
+              onPressed: () async {
+                String codeSanner =
+                    await BarcodeScanner.scan(); //barcode scnner
+                setState(() {
+                  qrCodeResult = codeSanner;
+                  _launchInBrowser(qrCodeResult);
+                });
+
+                // try{
+                //   BarcodeScanner.scan()    this method is used to scan the QR code
+                // }catch (e){
+                //   BarcodeScanner.CameraAccessDenied;   we can print that user has denied for the permisions
+                //   BarcodeScanner.UserCanceled;   we can print on the page that user has cancelled
+                // }
+              },
+              child: Text(
+                "Open Scanner",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.blue, width: 3.0),
+                  borderRadius: BorderRadius.circular(20.0)),
+            ),
+            FutureBuilder<void>(future: _launched, builder: _launchStatus),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //its quite simple as that you can use try and catch staatements too for platform exception
+}
